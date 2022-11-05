1229b8dabdc0326c759299f17cfffbe88f3ceb0d
Bhavesh <bhic2030@gmail.com>
2022-11-05 10:27:36 +0530

    add

--- components/Scanner.tsx
+++ components/Scanner.tsx
@@ -13,21 +13,44 @@ const QrReader = dynamic(
 
 const Scanner = () => {
   const [data, setData] = useState("No result");
-  const [ticketId, setTicketId] = useState<null | string>(null)
+  const [ticketId, setTicketId] = useState<null | string>(null);
 
   return (
     <div className={styles["container"]}>
       <div className={styles["scanner"]}>
         <QrReader
-          onResult={(result, error) => {
+          onResult={async (result, error) => {
             if (!!result) {
               console.log(result);
               setData(result.getText());
-              const head = result.getText().indexOf("Ticket id: ") + 11;
-              const tail = result.getText().length
-              const ticket_id = result.getText().substring(head, tail + 1)
-              setTicketId(ticket_id)
-              alert(`Ticket id: ${ticket_id}`);
+              const head =
+                result.getText().indexOf("Ticket id: ") === -1
+                  ? result.getText().indexOf("ticket id: ") + 11
+                  : result.getText().indexOf("Ticket id: ") + 11;
+              if (head === -1) {
+                return alert("Inavlid Ticket");
+              }
+              const tail = result.getText().length;
+              const ticket_id = result.getText().substring(head, result.getText().indexOf("Ticket id: ") === -1 ? tail - 1: tail + 1) ;
+              setTicketId(ticket_id.trim());
+              alert(`Ticket id: ${ticket_id.trim()}`);
+
+              const id = ticket_id
+
+              const res = await fetch(
+                `https://devfest2022tickets.scattrhq.repl.co/api/v1/counter_api`,
+                {
+                  method: "POST",
+                  body: JSON.stringify({ id }),
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                }
+              );
+
+              const data = res.json()
+
+              console.log(data)
             }
           }}
           constraints={{ facingMode: "environment" }}
